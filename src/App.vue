786d61d61c2ca83f371f380c5b25808ad318b5cb
Djordje Cvijic <djordjec@twodesperados.com>
2023-01-03 09:16:59 +0100

    Make it a vue project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <AkktaStudioOffers msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import AkktaStudioOffers from './components/AkktaStudioOffers.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AkktaStudioOffers
+  }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: "Proxima Nova";
+    src: url("@/assets/fonts/ProximaNova.otf");
+}
+
+html {
+    box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+    box-sizing: inherit;
+}
+
+p {
+    margin: 0;
+}
+
+body {
+    margin: 0;
+    padding: 0 0 32px 0;
+    background-color: white;
+    font-family: "Proxima Nova", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+</style>
